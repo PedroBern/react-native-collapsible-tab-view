ac0696b640e9b11280607c3ac6afbe5cd86e2ca9
Pedro Bern <pedrobermoreira@gmail.com>
2020-11-19 11:02:14 -0300

    feat: add collapsible tab view

---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+export { default as CollapsibleTabView } from './CollapsibleTabView';
+export { useCollapsibleScene } from './CollapsibleTabView';
+export type { Props as CollapsibleTabViewProps } from './CollapsibleTabView';
