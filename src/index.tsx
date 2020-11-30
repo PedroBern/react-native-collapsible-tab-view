e34a34b8ef2fb498be1afbbfb292154265ee7ece
Andrei Alecu <andrei.alecu@vecttor.co>
2020-11-30 16:24:58 +0200

    feat: createMaterialCollapsibleTopTabNavigator

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,6 @@ export type { Props as CollapsibleTabViewProps } from './CollapsibleTabView';
 
 export { default as useCollapsibleScene } from './useCollapsibleScene';
 export type { CollapsibleScenePropsAndRef } from './types';
+
+export { default as MaterialTopTabsCollapsibleTabView } from './MaterialTopTabsCollapsibleTabView';
+export { default as createMaterialCollapsibleTopTabNavigator } from './createMaterialCollapsibleTopTabNavigator';
