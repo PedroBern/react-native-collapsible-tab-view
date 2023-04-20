02a71d955827f1ea630228d5dd34b6564b2644a9
Luciano J. Bracco <lbracco@geojam.com>
2023-04-20 14:18:55 -0300

    feat: added example for FlashList

--- example/src/AnimatedHeader.tsx
+++ example/src/AnimatedHeader.tsx
@@ -8,7 +8,7 @@ import Animated, {
 } from 'react-native-reanimated'
 
 import { useCurrentTabScrollY } from '../../src/hooks'
-import ExampleComponent from './Shared/ExampleComponent'
+import ExampleComponent from './Shared/ExampleComponentFlashList'
 import ReText from './Shared/ReText'
 import { ExampleComponentType } from './types'
 
