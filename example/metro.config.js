6eb60f39693aa302b8537725be9d16e9a664b97c
Andrei Alecu <andreialecu@users.noreply.github.com>
2023-04-21 12:47:05 +0300

    chore: update example dependencies

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,27 +1,28 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config')
 const path = require('path')
+
 const extraNodeModules = {
-  // eslint-disable-next-line no-path-concat
-  'react-native-collapsible-tab-view': path.resolve(__dirname + '/../src'),
+  'react-native-collapsible-tab-view': path.resolve(`${__dirname}/../src`),
 }
-// eslint-disable-next-line no-path-concat
-const watchFolders = [path.resolve(__dirname + '/../src')]
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+
+const config = getDefaultConfig(__dirname)
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: false,
   },
-  resolver: {
-    extraNodeModules: new Proxy(extraNodeModules, {
-      get: (target, name) =>
-        //redirects dependencies referenced from src/ to local node_modules
-        name in target
-          ? target[name]
-          : path.join(process.cwd(), `node_modules/${name}`),
-    }),
-  },
-  watchFolders,
-}
+})
+
+config.resolver.extraNodeModules = new Proxy(extraNodeModules, {
+  get: (target, name) =>
+    //redirects dependencies referenced from src/ to local node_modules
+    name in target
+      ? target[name]
+      : path.join(process.cwd(), `node_modules/${name}`),
+})
+
+config.watchFolders = [path.resolve(`${__dirname}/../src`)]
+
+module.exports = config
