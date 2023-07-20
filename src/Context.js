9e9cea7e4551caf69546ca2ac9edc4e92a1b4941
George Kartalis <g.kartalis7@gmail.com>
2023-07-20 15:47:07 +0200

    chore: remove js files

--- src/Context.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-export const Context = React.createContext(undefined);
-export const TabNameContext = React.createContext(undefined);
--- src/MaterialTabBar/index.js
+++
@@ -1,2 +0,0 @@
-export { MaterialTabBar, TABBAR_HEIGHT } from './TabBar';
-export { MaterialTabItem } from './TabItem';
