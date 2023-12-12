3dbca9c9b59e2a258d0b6b615f80f886c981d444
David Leuliette <dleuliette@gmail.com>
2023-12-12 16:41:17 +0100

    chore: bump demo code to expo 49.0.21

--- example/android/app/src/release/java/com/rnctvexample/ReactNativeFlipper.java
+++ example/android/app/src/release/java/com/rnctvexample/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.reactnativecollapsibletabviewdemos;
+package com.rnctvexample;
 
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
